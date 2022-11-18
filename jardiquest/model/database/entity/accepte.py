9fe0bfb6ff3f45dcaabf4ce61a5ce661eb40a5ac
Kallu-A <lucas.aries@protonmail.com>
2022-11-18 10:40:36 +0100

    fix: missing table import

--- jardiquest/model/database/entity/accepte.py
+++ jardiquest/model/database/entity/accepte.py
@@ -4,7 +4,7 @@ class Accepte(db.Model):
     __tableName__= "accepte"
     
     idUser = db.Column(db.ForeignKey("user.idUser"), primary_key=True)
-    idQuete = db.Column(db.ForeignKey("quete.idQuete"), primary_key=True),
+    idQuete = db.Column(db.ForeignKey("quete.idQuete"), primary_key=True)
     ended = db.Column(db.Boolean(),default=False)
 
     user = db.relationship("User", backref="quete")
