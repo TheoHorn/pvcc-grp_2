90435246c49da4fc49496dfb9bb81e113b08c14f
Kallu-A <lucas.aries@protonmail.com>
2022-11-24 23:33:25 +0100

    add: navbar

--- jardiquest/controller/common.py
+++ jardiquest/controller/common.py
@@ -6,7 +6,8 @@ from jardiquest.controller import app
 
 @app.get('/')
 def home():
-    return render_template('home.html')
+    from jardiquest.model.path.common_model import home_model
+    return home_model()
 
 
 @app.get('/profile')
--- jardiquest/model/database/entity/user.py
+++ jardiquest/model/database/entity/user.py
@@ -4,6 +4,7 @@ import re
 
 regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
 
+
 class User(db.Model):
     __tablename__ = "user"
 
