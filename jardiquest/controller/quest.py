e953eba7278538ea1ee123323ce98b8993a6e35d
Paul Deveaux <paul.deveaux1@gmail.com>
2022-12-16 23:44:44 +0100

    change in files

--- jardiquest/controller/quest.py
+++ jardiquest/controller/quest.py
@@ -1,46 +1,45 @@
-from flask import request, render_template, redirect, url_for, session
-from jardiquest.model.database.entity.quete import Quete
-from jardiquest.model.database.entity.jardin import Jardin
-from jardiquest.model.database.entity.user import User
-from datetime import date
+from flask import render_template, redirect, url_for, session
 from . import app
 
 
-# ------------------------------------------ Garden quests ------------------------------------------
+def get_connected_user_id():
+    """Returns the id of the connected user or redirect to login page if not connected"""
+    user_id = session.get("_user_id")
+    if user_id is None:
+        return redirect(url_for("controller.login"))
+    else:
+        return user_id
+
+
 @app.get("/garden/<int:id_garden>/quests")
 def list_garden_id_quests(id_garden):
     """List all quests of a specific garden"""
-    print(session)
-    garden = Jardin.query.get(id_garden)
-    quests = Quete.query.filter_by(id_jardin=id_garden).all()
-    return render_template("quests_list.html", quests=quests, today = date.today(), garden = garden)
+    # TODO Soit delete, soit n'autoriser qu'aux modérateurs
+    from jardiquest.model.path.quest_model import list_garden_id_quest_model
+    return list_garden_id_quest_model(id_garden)
 
 
 @app.get("/garden/quests")
 def list_garden_quests():
     """List all quests of the garden"""
-    user_id = session.get("_user_id")
-    if user_id is None:
-        return redirect(url_for("controller.login"))
-    else:
-        from jardiquest.model.path.quest_model import list_garden_quest_model
-        return list_garden_quest_model(user_id)
+    user_id = get_connected_user_id()
+    from jardiquest.model.path.quest_model import list_garden_quest_model
+    return list_garden_quest_model(user_id)
 
 
-
-# ------------------------------------------ User quests ------------------------------------------
 @app.get("/my_quests")
-def list_my_quests():
+def list_user_quests():
     """List all quests accepted by the user"""
-    id = 1 # TODO get the id of the connected user
-    quests = Quete.query.filter_by(user=id).all()
-    pass
+    user_id = get_connected_user_id
+    from jardiquest.model.path.quest_model import list_my_quest_model
+    return list_my_quest_model(user_id)
 
 
 
-# ------------------------------------------ Quests Details ------------------------------------------
 @app.get("/quest/<int:quest_id>")
 def display_quest(quest_id):
     """Display a quest with a specific id"""
-    quest = Quete.query.get(quest_id)
-    return quest
+    from jardiquest.model.path.quest_model import display_quest_model
+    return display_quest_model(quest_id)
+
+
