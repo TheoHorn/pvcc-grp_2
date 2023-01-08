ed81ffb4970dab79e5f7f78744eb6daff9178d1f
Kallu-A <lucas.aries@protonmail.com>
2023-01-08 14:45:51 +0100

    fix: database separation

--- jardiquest/tests/conftest.py
+++ jardiquest/tests/conftest.py
@@ -8,18 +8,18 @@ from jardiquest.setup_flask import create_app, ROOT_DIR
 # Allow to not write the lines in the test_function everywhere but instead just pass testing_client in argument
 @pytest.fixture(scope='module')
 def app():
-    flask_app = create_app()
+    flask_app = create_app(True)
     with flask_app.test_client() as app:
         with flask_app.app_context():
             yield app
 
+
 @pytest.fixture(scope='class', autouse=True)
 def database(app):
-    f = open( os.path.join(ROOT_DIR, 'tests/dataset/dataset.sqlite') )
+    f = open(os.path.join(ROOT_DIR, 'tests/dataset/dataset.sqlite'))
     db.create_all()
-    for l in f.readlines():
-        print(l)
-        db.session.connection().execute(l)
+    for line in f.readlines():
+        db.session.connection().execute(line)
         db.session.commit()
     f.close()
     yield db
